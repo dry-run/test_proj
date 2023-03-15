1f7453295b03ec9e3a5e99cdd8b326a978171262
Kalpana Reddy <Kalpana.c@infomagnus.com>
2023-03-15 21:57:48 +0530

    Updated Jenkins config script

--- jenkins-pipelines/updating-ghes2ghec-config-info-in-jobs.sh
+++ jenkins-pipelines/updating-ghes2ghec-config-info-in-jobs.sh
@@ -11,17 +11,18 @@ while IFS= read -r JOB_NAME; do
     JOB_NAME=${JOB_NAME/////job/}
     JOB_NAME=${JOB_NAME// /%20}
     echo "JOB_NAME $JOB_NAME"
-    JOB_URL="http://$JENKINS_URL/job/$JOB_NAME/config.xml"
+    JOB_URL="https://$JENKINS_URL/job/$JOB_NAME/config.xml"
     echo "JOB_URL $JOB_URL"
-    curl --user "$JENKINS_USER":"$JENKINS_API_TOKEN" -X GET "$JOB_URL" -o "$JOB_NAME_FILE".xml
+    curl -k --user "$JENKINS_USER":"$JENKINS_API_TOKEN" -X GET "$JOB_URL" -o "$JOB_NAME_FILE".xml
     lines_with_ghes_link=$(cat "$JOB_NAME_FILE".xml | grep "github.fleet.ad")
 
     # Comment this block if you don't want to update the config
     sed -i "s/$GHES_URL\/$OLD_ORG/$GHEC_URL\/$NEW_ORG/g" "$JOB_NAME_FILE".xml
     sed -i "s/$GHES_URL:$OLD_ORG/$GHEC_URL:$NEW_ORG/g" "$JOB_NAME_FILE".xml
     sed -i "s/http:\/\/$GHEC_URL/https:\/\/$GHEC_URL/g" "$JOB_NAME_FILE".xml
+    sed -i "s/git@$GHEC_URL:/https:\/\/$GHEC_URL\//g" "$JOB_NAME_FILE".xml
     sed -i "s/<credentialsId>.*<\/credentialsId>/<credentialsId>$CRED_ID<\/credentialsId>/g" "$JOB_NAME_FILE".xml
-    curl --user "$JENKINS_USER":"$JENKINS_API_TOKEN" -X POST "$JOB_URL" -H 'Content-Type: application/xml' --data-binary "@$JOB_NAME_FILE.xml"
+    curl -k --user "$JENKINS_USER":"$JENKINS_API_TOKEN" -X POST "$JOB_URL" -H 'Content-Type: application/xml' --data-binary "@$JOB_NAME_FILE.xml"
     # ----
 
     echo "$JOB_URL|$lines_with_ghes_link" >>jenkins_dependencies_report.txt
